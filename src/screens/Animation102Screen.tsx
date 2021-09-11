7d670fa975580cb6110e8a906eaa31f249bfae4a
cedgo <cedgo1997@gmail.com>
2021-09-11 19:17:50 -0400

    Drag box animated

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,17 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { Animated, Button, PanResponder, StyleSheet, Text, View } from 'react-native';
 
 export const Animation102Screen = () => {
+
+    const { current: pan } = useRef(new Animated.ValueXY());
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: Animated.event([
+            null,
+            {
+                dx: pan.x,
+                dy: pan.y,
+            },
+
+        ], {
+            useNativeDriver: false
+        }),
+        onPanResponderRelease: () => {
+            Animated.spring(
+                pan,
+                {
+                    toValue: { x: 0, y: 0 },
+                    useNativeDriver: true
+                },
+            ).start()
+        }
+    })
+
     return (
-        <View style={{ flex: 1 }}>
-            <View style={styles.purpleBox}></View>
+        <View style={styles.container}>
+            <Animated.View
+                {...panResponder.panHandlers}
+                style={{
+                    ...styles.greenBox, transform: [{
+                        translateX: pan.getLayout().left,
+                    }, {
+                        translateY: pan.getLayout().top
+                    }]
+                }}>
+            </Animated.View>
         </View>
     );
 };
 
+
 const styles = StyleSheet.create({
-    purpleBox: {
-        backgroundColor: '#5856D6',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    greenBox: {
+        backgroundColor: '#12DF7A',
         width: 150,
         height: 150
     },
