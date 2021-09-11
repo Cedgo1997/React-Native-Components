5360225e87f8c9f31265bc60486e525f9125f749
cedgo <cedgo1997@gmail.com>
2021-09-10 23:16:14 -0400

    Styling list

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { MenuItem } from '../interfaces/interfaces';
 
 interface Props {
@@ -8,8 +10,22 @@ interface Props {
 
 export const FlatListMenuItem = ({ menuItem }: Props) => {
     return (
-        <View>
-            <Text>{menuItem.name} - {menuItem.icon}</Text>
-        </View>
+        <TouchableOpacity activeOpacity={0.8}>
+            <View style={styles.container}>
+                <Icon name={menuItem.icon} size={20} />
+                <Text style={styles.textItem}>{menuItem.name}</Text>
+                <View style={{ flex: 1 }} />
+                <Icon name="arrow-forward-circle-outline" size={25} />
+            </View>
+        </TouchableOpacity>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row'
+    },
+    textItem: {
+        marginHorizontal: 5
+    },
+});
