cd0e33a314f2893eafb657447ac0faa9d0d6a641
cedgo <cedgo1997@gmail.com>
2021-09-10 22:00:21 -0400

    Using FlatList

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import { FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+interface MenuItem {
+    name: string;
+    icon: string;
+    component: string
+}
+
+const menuItems = [
+    { name: 'Animation 101', icon: 'cube-outline', component: 'Animation101Screen' }
+]
+
 export const HomeScreen = () => {
+
+    const renderMenuItem = (menuItem: MenuItem) => {
+        return (
+            <View>
+                <Text>{menuItem.name} - {menuItem.icon}</Text>
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Icon name="star-outline" color='grey' size={30} />
+        <View style={{ flex: 1 }}>
+            <FlatList
+                data={menuItems}
+                keyExtractor={(item) => item.name}
+                renderItem={({ item }) => renderMenuItem(item)} />
         </View>
     );
 };
