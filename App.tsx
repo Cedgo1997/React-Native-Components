67836b7e012a966a305e51ff39745ccec873d7f5
cedgo <cedgo1997@gmail.com>
2021-09-10 20:04:03 -0400

    Setting up app. Stack Navigation Icons from Ionicons

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Hola Mundo</Text>
+    </NavigationContainer>
+  );
+};
