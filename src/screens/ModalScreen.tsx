f3162dab337aa70477ea6878f2f3bba966ecbdfa
cedgo <cedgo1997@gmail.com>
2021-09-15 00:45:33 -0400

    Modal style

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -15,23 +15,28 @@ export const ModalScreen = () => {
                 animationType='slide'
                 transparent={true}
                 visible={isVisible}
-                onRequestClose={() => {
-                    Alert.alert("Modal Closed", "Modal now is closed, you can do whatever you want.");
-                    setIsVisible(false);
-                }}
             >
                 {/* Modal background */}
                 <View style={
                     {
                         flex: 1,
                         backgroundColor: 'rgba(0,0,0,0.3)',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        shadowOffset: { width: 0, height: 10 },
+                        shadowOpacity: 0.25,
+                        elevation: 10,
+                        borderRadius: 5
                     }
                 }>
                     {/* Modal body */}
-                    <View style={{ backgroundColor: 'white' }} >
-                        <HeaderTitle title="Modal" />
-                        <Text>Modal Body</Text>
-                        <Button title="Close" onPress={() => setIsVisible(false)} />
+                    <View style={{ backgroundColor: 'white', width: 200, height: 200, justifyContent: 'center', alignItems: 'center' }} >
+                        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Modal</Text>
+                        <Text style={{ fontSize: 16, fontWeight: '300', marginBottom: 20 }}>Modal Body</Text>
+                        <Button
+                            title="Close"
+                            onPress={() => setIsVisible(false)}
+                        />
                     </View>
                 </View>
             </Modal>
