510d578f3413f424ccee653925ee461facee45e2
cedgo <cedgo1997@gmail.com>
2021-09-14 21:44:44 -0400

    Refresh misc

--- src/screens/PullToRefresh.tsx
+++ src/screens/PullToRefresh.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { RefreshControl, View } from 'react-native';
+import { RefreshControl, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
@@ -7,11 +7,13 @@ import { styles } from '../theme/appTheme';
 export const PullToRefresh = () => {
 
     const [refreshing, setRefreshing] = useState(false);
+    const [data, setData] = useState<string>();
 
     const onRefresh = () => {
         setRefreshing(true);
         setTimeout(() => {
             console.log("End");
+            setData("Hello, World!")
             setRefreshing(false);
         }, 1500);
     }
@@ -21,11 +23,16 @@ export const PullToRefresh = () => {
                 <RefreshControl
                     refreshing={refreshing}
                     onRefresh={onRefresh}
+                    progressBackgroundColor="lightblue"
+                    colors={['white', 'red', 'orange']}
+                    title="Refreshing" /* IOS */
+                    titleColor="black" /* IOS */
                 />
             }
         >
             <View style={styles.globalMargin}>
                 <HeaderTitle title="Pull to Refresh" />
+                {data && <Text>{data}</Text>}
             </View>
         </ScrollView>
     );
