e1ff544c98f0ba7ae4d06a73c02f0aed3ca39883
cedgo <cedgo1997@gmail.com>
2021-09-15 23:01:56 -0400

    Carousel Pagination

--- src/screens/SlidersScreen.tsx
+++ src/screens/SlidersScreen.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Carousel from 'react-native-snap-carousel';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 import { Slide, sliderItems } from '../data/sliderData';
 
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
 
 export const SlidersScreen = () => {
+
+    const [slideIndex, setSlideIndex] = useState(0);
 
     const renderItem = (item: Slide) => {
         return (
@@ -25,6 +27,25 @@ export const SlidersScreen = () => {
         );
     }
 
+    const pagination = () => {
+        return (
+            <Pagination
+                dotsLength={sliderItems.length}
+                activeDotIndex={slideIndex}
+                containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}
+                dotStyle={{
+                    width: 10,
+                    height: 10,
+                    borderRadius: 5,
+                    marginHorizontal: 8,
+                    backgroundColor: 'rgba(255, 255, 255, 0.92)'
+                }}
+                inactiveDotOpacity={0.4}
+                inactiveDotScale={0.6}
+            />
+        );
+    }
+
     return (
         <SafeAreaView
             style={{ flex: 1, paddingTop: 50 }}
@@ -34,7 +55,9 @@ export const SlidersScreen = () => {
                 renderItem={({ item }) => renderItem(item)}
                 sliderWidth={screenWidth}
                 itemWidth={screenWidth}
+                onSnapToItem={(index) => setSlideIndex(index)}
             />
+            {pagination()}
         </SafeAreaView>
     );
 };
